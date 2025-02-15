c59feee9cb1ff2d221158b7aef4e331da6429668
nidhisingh5958 <nidhisingh5958@gmail.com>
2025-02-15 19:25:19 +0530

    initial

---
+++ demo.java
@@ -0,0 +1,15 @@
+import java.utils.Arrays;
+
+public class demo {
+    public static void main (String[] args){
+        int age = 20;
+        System.out.println();
+
+        int[] marks = new int[3];
+        marks[0]=98;
+        marks[1]=90;
+        marks[2]=95;
+        System.out.println(marks);
+        System.out.println(marks[0]);
+    }
+}
